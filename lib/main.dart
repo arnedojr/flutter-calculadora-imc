136276b55bacbe1a4edcf4ab0da0b22d5610885e
Arnedo <arnedojr@gmail.com>
2020-06-01 18:24:42 -0300

    teste de commit - ferramenta de controle de versao no visual studio code

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
+  //teste controle de vers
 
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
